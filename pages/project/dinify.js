ad904baf9cd98bac27ac8e2b5475ba0b5ade671d
Lukáš Vaněk <lukas@sinesquare.com>
2020-09-24 18:02:03 +0200

    Dinify content 2

--- pages/project/dinify.js
+++ pages/project/dinify.js
@@ -30,8 +30,11 @@ export default () => {
           guests could order food from table in their native language. That
           could have been achieved by scanning QR code with their phone using
           our{' '}
-          <a href="https://en.wikipedia.org/wiki/Progressive_web_application">
-            PWA app
+          <a
+            href="https://en.wikipedia.org/wiki/Progressive_web_application"
+            target="_blank"
+          >
+            Progressive Web App
           </a>
           , so the user wouldn't have to install anything. We especially focused
           on minimizing steps needed to start using our app.
@@ -44,23 +47,46 @@ export default () => {
           restaurant. After uneasy decision of our founder, we moved to European
           market, focusing on cities with high tourism, namely Prague, Madrid
           and Barcelona.
+          <br />
+          We participated in gastroturism accelerator in San Sebastian.
+          <br />
+          In 2020 during the pandemic situation, our founder had to leave Spain
+          and come to Korea, the country of his origin, therefore he had to cut
+          the business development in Spain. In order to save the project we
+          tried to adapt to different market once again, the restaurants of
+          Seoul.
         </p>
         <h3>My role</h3>
         <p>
           The first thing I was working on was developing web app for waiters,
           so called Waiterboard, I picked React/Redux with{' '}
-          <a href="https://redux-observable.js.org/">Observables</a> and{' '}
-          <a href="https://styled-components.com/">styled-components</a> as a
-          stack. The backend was written in Java and I found it rather
+          <a href="https://redux-observable.js.org/" target="_blank">
+            Observables
+          </a>{' '}
+          and{' '}
+          <a href="https://styled-components.com/" target="_blank">
+            styled-components
+          </a>{' '}
+          as a stack. The backend was written in Java and I found it rather
           suboptimal, so I build my own microservice dedicated to the Waiteboard
-          in Node.js using <a href="https://expressjs.com/">Express</a> creating
-          simple REST API. The main database was MySQL and I used{' '}
-          <a href="https://sequelize.org/">Sequelize</a> as ORM.
+          in Node.js using{' '}
+          <a href="https://expressjs.com/" target="_blank">
+            Express
+          </a>{' '}
+          creating simple REST API. The main database was MySQL and I used{' '}
+          <a href="https://sequelize.org/" target="_blank">
+            Sequelize
+          </a>{' '}
+          as ORM.
           <br />
           The second thing I developed was Dashboard for managers and owners of
           the restaurant, where they could manage their menu, translations of
           menu and other settings. The stack was very similar. Later we started
-          using <a href="https://material-ui.com/">Material UI</a> library.
+          using{' '}
+          <a href="https://material-ui.com/" target="_blank">
+            Material UI
+          </a>{' '}
+          library.
           <br />
           The user app was initially separately written natively for Android and
           iOS. For both development and user experience reasons we decided to
@@ -70,9 +96,21 @@ export default () => {
           The rest of old Java backend was rewritten in PHP with Laravel by my
           colleague. Which after some time turned out as a bad idea as we ran
           into serious performance issues. So I initiated incremental migration
-          to Node.js backend with <a href="https://nestjs.com/">Nest.js</a>.
-          This made all requests faster by factor of ten.
+          to Node.js backend with{' '}
+          <a href="https://nestjs.com/" target="_blank">
+            Nest.js
+          </a>
+          . This made all requests faster by factor of ten.
           <br />
+          Lastly during the trial of adaptation to Korean market I worked on new{' '}
+          <a href="https://github.com/necolas/react-native-web" target="_blank">
+            React Native Web
+          </a>{' '}
+          app for waiters, where I used{' '}
+          <a href="https://rematch.netlify.app/" target="_blank">
+            Rematch
+          </a>{' '}
+          for state management.
         </p>
 
         <Divider />
